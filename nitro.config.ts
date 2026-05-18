9961d0dd4826fde6b1b601ebad8cae972b249799
peak111-ai <28561311@qq.com>
2026-05-18 09:53:35 +0800

    Update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,70 +1,15 @@
-import process from "node:process"
-import { join } from "node:path"
-import viteNitro from "vite-plugin-with-nitro"
-import { RollopGlob } from "./tools/rollup-glob"
-import { projectDir } from "./shared/dir"
+import { defineNitroConfig } from 'nitropack/config'
 
-const nitroOption: Parameters<typeof viteNitro>[0] = {
-  experimental: {
-    database: true,
+export default defineNitroConfig({
+  preset: 'cloudflare-pages',
+  srcDir: 'server',
+  compatibilityDate: '2024-11-28',
+  output: {
+    dir: 'dist/output',
+    publicDir: 'dist/output/public'
   },
-  rollupConfig: {
-    plugins: [RollopGlob()],
-  },
-  sourceMap: false,
-  database: {
-    default: {
-      connector: "better-sqlite3",
-    },
-  },
-  devDatabase: {
-    default: {
-      connector: "better-sqlite3",
-    },
-  },
-  imports: {
-    dirs: ["server/utils", "shared"],
-  },
-  preset: "node-server",
-  alias: {
-    "@shared": join(projectDir, "shared"),
-    "#": join(projectDir, "server"),
-  },
-}
-
-if (process.env.VERCEL) {
-  nitroOption.preset = "vercel-edge"
-  // You can use other online database, do it yourself. For more info: https://db0.unjs.io/connectors
-  nitroOption.database = undefined
-  // nitroOption.vercel = {
-  //   config: {
-  //     cache: []
-  //   },
-  // }
-} else if (process.env.CF_PAGES) {
-  nitroOption.preset = "cloudflare-pages"
-  nitroOption.unenv = {
-    alias: {
-      "safer-buffer": "node:buffer",
-    },
+  serverDir: 'dist/output/server',
+  cloudflare: {
+    pagesFunctionRoutes: true
   }
-  nitroOption.database = {
-    default: {
-      connector: "cloudflare-d1",
-      options: {
-        bindingName: "NEWSNOW_DB",
-      },
-    },
-  }
-} else if (process.env.BUN) {
-  nitroOption.preset = "bun"
-  nitroOption.database = {
-    default: {
-      connector: "bun-sqlite",
-    },
-  }
-}
-
-export default function () {
-  return viteNitro(nitroOption)
-}
+})
