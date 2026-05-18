1007c40a9a570b55b8215a959925fb375b286609
peak111-ai <28561311@qq.com>
2026-05-18 17:51:00 +0800

    Update pre-sources.ts

--- shared/pre-sources.ts
+++ shared/pre-sources.ts
@@ -24,27 +24,30 @@ export const originSources = {
       },
     },
   },
-  "nanchangAudit": {
-    name: "南昌市审计局",
-    color: "blue",
-    home: "http://sjj.nc.gov.cn",
-    column: "news",
-    type: "realtime"
-  },
-  "jiangxiAudit": {
-    name: "江西省审计厅",
-    color: "green",
-    home: "https://audit.jiangxi.gov.cn",
-    column: "news",
-    type: "realtime"
-  },
-  "auditGov": {
-    name: "审计署",
-    color: "red",
-    home: "https://www.audit.gov.cn",
-    column: "news",
-    type: "realtime"
-  },
+"nanchangAudit": {
+  name: "南昌市审计局",
+  title: "最新动态",     
+  color: "blue",
+  home: "https://sjj.nc.gov.cn",  
+  column: "news",
+  type: "realtime"
+},
+"jiangxiAudit": {
+  name: "江西省审计厅",
+  title: "最新动态",    
+  color: "green",
+  home: "https://audit.jiangxi.gov.cn",
+  column: "news",
+  type: "realtime"
+},
+"auditGov": {
+  name: "审计署",
+  title: "最新动态",   
+  color: "red",
+  home: "https://www.audit.gov.cn",
+  column: "news",
+  type: "realtime"
+},
   "zhihu": {
     name: "知乎",
     type: "hottest",
