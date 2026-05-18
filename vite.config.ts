ea532354b74e6af432f650df82d262773120860f
peak111-ai <28561311@qq.com>
2026-05-18 10:51:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { join } from "node:path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import unocss from "unocss/vite"
 import unimport from "unimport/unplugin"
 import dotenv from "dotenv"
-import nitro from "./nitro.config"
 import { projectDir } from "./shared/dir"
 import pwa from "./pwa.config"
 
@@ -21,10 +19,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    TanStackRouterVite({
-      // error with auto import and vite-plugin-pwa
-      // autoCodeSplitting: true,
-    }),
     unimport.vite({
       dirs: ["src/hooks", "shared", "src/utils", "src/atoms"],
       presets: ["react", {
@@ -40,6 +34,5 @@ export default defineConfig({
     unocss(),
     react(),
     pwa(),
-    nitro(),
   ],
 })
